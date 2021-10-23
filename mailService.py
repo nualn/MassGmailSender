94b9ba79cf365618dd186723d24f863bdc473025
nuutti <nuuttinikkola1@gmail.com>
2021-10-23 23:19:06 +0300

    Restructured mailService into several functions to make the code more readable

--- mailService.py
+++ mailService.py
@@ -8,9 +8,7 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-def send_mail(fromaddr, toaddr, subject, body, filename):
-    access_token, expires_in = oauth.refresh_authorization(config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET, config.GOOGLE_REFRESH_TOKEN)
-    auth_string = oauth.generate_oauth2_string(config.SENDER, access_token, as_base64=True)
+def createMessage(fromaddr, toaddr, subject, body, filename): 
     # Create a multipart message and set headers
     message = MIMEMultipart()
     message["From"] = fromaddr
@@ -20,35 +18,51 @@ def send_mail(fromaddr, toaddr, subject, body, filename):
     # Add body to email
     message.attach(MIMEText(body, "plain"))
 
+    # If there is a file to be attached, add it to the message
     if filename:
-        filepath = config.FILE_FOLDER + filename  # In same directory as script
+        filepath = config.FILE_FOLDER + filename 
+        addAttachment(filepath, message)
 
-        # Open PDF file in binary mode
-        with open(filepath, "rb") as attachment:
-            # Add file as application/octet-stream
-            # Email client can usually download this automatically as attachment
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
+    return message
+    
+    
 
-        # Encode file in ASCII characters to send by email    
-        encoders.encode_base64(part)
+def addAttachment(filepath, message):
 
-        # Add header as key/value pair to attachment part
-        part.add_header(
-            "Content-Disposition",
-            f"attachment; filename= {filepath}",
-        )
+    # Open PDF file in binary mode
+    with open(filepath, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
-        # Add attachment to message and convert message to string
-        message.attach(part)
-        
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filepath}",
+    )
+
+    # Add attachment to message and convert message to string
+    message.attach(part)
+
+
+def send_mail(fromaddr, toaddr, subject, body, filename):
+    access_token, expires_in = oauth.refresh_authorization(config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET, config.GOOGLE_REFRESH_TOKEN)
+    auth_string = oauth.generate_oauth2_string(config.SENDER, access_token, as_base64=True)
+    
+    # Create the email 
+    message = createMessage(fromaddr, toaddr, subject, body, filename)
     text = message.as_string()
     
-    server = smtplib.SMTP('smtp.gmail.com:587')
+    # Start server with credentials
+    server =  smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo(config.GOOGLE_CLIENT_ID)
     server.starttls()
     server.docmd('AUTH', 'XOAUTH2 ' + auth_string)
-    
+    # Send the email
     server.sendmail(fromaddr, toaddr, text)
     # Close server
     server.quit()
