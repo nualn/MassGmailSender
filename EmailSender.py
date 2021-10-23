d1370ae37d765baa7560b9f6e2158f2071b1ef90
nuutti <nuuttinikkola1@gmail.com>
2021-10-23 20:28:53 +0300

    Removed user inputed variables from EmailSender.py

--- EmailSender.py
+++ EmailSender.py
@@ -22,28 +22,8 @@ from email import encoders
 from email.mime.base import MIMEBase
 import xlrd
 
-# Mail parameters
-
-# Sender's email address
-SENDER = 'email@gmail.com'
-# Subject of the email
-SUBJECT = 'Place holder'
-# Body of the email
-MSG_BODY = 'Place holder'
-# Folder with attachments
-FILE_FOLDER = 'files/'
-# Workbook that connects the mail addresses to files 
-XLS_LOC = 'mailsAndFiles.xls'
-
 GOOGLE_ACCOUNTS_BASE_URL = 'https://accounts.google.com'
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
-
-# Parameters from Google's API development
-GOOGLE_CLIENT_ID = ''
-GOOGLE_CLIENT_SECRET = ''
-# Token from when you first run the script
-GOOGLE_REFRESH_TOKEN = ''
-
 
 def command_to_url(command):
     return '%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, command)
