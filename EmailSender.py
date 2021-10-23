a59bb884bcfcee155ae676b3e5b018b36dbd5a13
nuutti <nuuttinikkola1@gmail.com>
2021-10-23 21:23:24 +0300

    Restructured oauth functionality to oauth.py

--- EmailSender.py
+++ EmailSender.py
@@ -1,12 +1,4 @@
-"""
-Adapted from:
-https://github.com/google/gmail-oauth2-tools/blob/master/python/oauth2.py
-https://developers.google.com/identity/protocols/OAuth2
 
-1. Generate and authorize an OAuth2 (generate_oauth2_token)
-2. Generate a new access tokens using a refresh token(refresh_token)
-3. Generate an OAuth2 string to use for login (access_token)
-"""
 import config
 
 import base64
@@ -22,98 +14,7 @@ from email.mime.text import MIMEText
 from email import encoders
 from email.mime.base import MIMEBase
 import xlrd
-
-
-GOOGLE_ACCOUNTS_BASE_URL = 'https://accounts.google.com'
-REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
-
-
-
-def command_to_url(command):
-    return '%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, command)
-
-
-def url_escape(text):
-    return urllib.parse.quote(text, safe='~-._')
-
-
-def url_unescape(text):
-    return urllib.parse.unquote(text)
-
-
-def url_format_params(params):
-    param_fragments = []
-    for param in sorted(params.items(), key=lambda x: x[0]):
-        param_fragments.append('%s=%s' % (param[0], url_escape(param[1])))
-    return '&'.join(param_fragments)
-
-
-def generate_permission_url(client_id, scope='https://mail.google.com/'):
-    params = {}
-    params['client_id'] = client_id
-    params['redirect_uri'] = REDIRECT_URI
-    params['scope'] = scope
-    params['response_type'] = 'code'
-    return '%s?%s' % (command_to_url('o/oauth2/auth'), url_format_params(params))
-
-
-def call_authorize_tokens(client_id, client_secret, authorization_code):
-    params = {}
-    params['client_id'] = client_id
-    params['client_secret'] = client_secret
-    params['code'] = authorization_code
-    params['redirect_uri'] = REDIRECT_URI
-    params['grant_type'] = 'authorization_code'
-    request_url = command_to_url('o/oauth2/token')
-    response = urllib.request.urlopen(request_url, urllib.parse.urlencode(params).encode('UTF-8')).read().decode('UTF-8')
-    return json.loads(response)
-
-
-def call_refresh_token(client_id, client_secret, refresh_token):
-    params = {}
-    params['client_id'] = client_id
-    params['client_secret'] = client_secret
-    params['refresh_token'] = refresh_token
-    params['grant_type'] = 'refresh_token'
-    request_url = command_to_url('o/oauth2/token')
-    response = urllib.request.urlopen(request_url, urllib.parse.urlencode(params).encode('UTF-8')).read().decode('UTF-8')
-    return json.loads(response)
-
-
-def generate_oauth2_string(username, access_token, as_base64=False):
-    auth_string = 'user=%s\1auth=Bearer %s\1\1' % (username, access_token)
-    if as_base64:
-        auth_string = base64.b64encode(auth_string.encode('ascii')).decode('ascii')
-    return auth_string
-
-
-def test_imap(user, auth_string):
-    imap_conn = imaplib.IMAP4_SSL('imap.gmail.com')
-    imap_conn.debug = 4
-    imap_conn.authenticate('XOAUTH2', lambda x: auth_string)
-    imap_conn.select('INBOX')
-
-
-def test_smpt(user, base64_auth_string):
-    smtp_conn = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp_conn.set_debuglevel(True)
-    smtp_conn.ehlo('test')
-    smtp_conn.starttls()
-    smtp_conn.docmd('AUTH', 'XOAUTH2 ' + base64_auth_string)
-
-
-def get_authorization(google_client_id, google_client_secret):
-    scope = "https://mail.google.com/"
-    print('Navigate to the following URL to auth:', generate_permission_url(google_client_id, scope))
-    authorization_code = input('Enter verification code: ')
-    response = call_authorize_tokens(google_client_id, google_client_secret, authorization_code)
-    return response['refresh_token'], response['access_token'], response['expires_in']
-
-
-def refresh_authorization(google_client_id, google_client_secret, refresh_token):
-    response = call_refresh_token(google_client_id, google_client_secret, refresh_token)
-    return response['access_token'], response['expires_in']
-
+import oauth
 
 def send_mail(fromaddr, toaddr, subject, body, filename):
     
@@ -158,7 +59,7 @@ def send_mail(fromaddr, toaddr, subject, body, filename):
 if __name__ == '__main__':
     if config.GOOGLE_REFRESH_TOKEN is None:
         print('No refresh token found, obtaining one')
-        refresh_token, access_token, expires_in = get_authorization(config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET)
+        refresh_token, access_token, expires_in = oauth.get_authorization(config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET)
         print('Set the following as your GOOGLE_REFRESH_TOKEN:', refresh_token)
         exit()
 
@@ -175,8 +76,8 @@ if __name__ == '__main__':
     sheet = wb.sheet_by_index(0)
 
     ### Moved connection opening calls here so they're not called for each mail.
-    access_token, expires_in = refresh_authorization(config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET, config.GOOGLE_REFRESH_TOKEN)
-    auth_string = generate_oauth2_string(config.SENDER, access_token, as_base64=True)
+    access_token, expires_in = oauth.refresh_authorization(config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET, config.GOOGLE_REFRESH_TOKEN)
+    auth_string = oauth.generate_oauth2_string(config.SENDER, access_token, as_base64=True)
 
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo(config.GOOGLE_CLIENT_ID)
